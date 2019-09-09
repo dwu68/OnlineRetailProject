e4b94b3788990f8901e4fe92bfe302f022f1acaa
dwu68 <dongli.pzh@gmail.com>
2019-09-09 16:58:01 -0400

    first submission

---
+++ submission/app.js
@@ -0,0 +1,39 @@
+function nameCheck(){
+    //clear for resubmits
+    document.getElementById("welcome_msg").innerHTML = "";
+
+    var name = document.getElementById('Name').value;
+    var check = confirm("Is " + name + " your name?");
+    if (check == true){
+        document.getElementById("welcome_msg").innerHTML = name + ", welcome to Digi-Safari Git workshop!";
+    }
+    else{
+        alert("Please enter full name.");  
+        document.getElementById("welcome_msg").innerHTML = "Welcome!";     
+    }
+}
+
+function checkLeapYear() {
+    //clear for resubmits
+    document.getElementById("result").innerHTML = "";
+
+    var year = document.getElementById("year").value;
+    var answer = "Not a leap year"
+
+    if (year % 4 == 0) {
+        answer = "It's a leap year!";
+    }
+    if (year % 100 == 0) {
+        answer = "Not a leap year";
+    }
+    if (year % 100 == 0 && year % 400 == 0) {
+        answer = "It's a leap year!";
+    }
+
+
+
+    document.getElementById("result").innerHTML = answer;
+
+    // If the year can be evenly divided by 100, it is NOT a leap year, unless;
+    // The year is also evenly divisible by 400. Then it is a leap year.
+}
